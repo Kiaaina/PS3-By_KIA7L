08a5035d8ea35c946d0d8add36637a06e743e49e
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-27 17:13:38 -1000

    Update PS3_Boot.cmd

--- PS3_Boot.cmd
+++ PS3_Boot.cmd
@@ -3,10 +3,11 @@ cd /
 cls
 mode 40,25
 title "PS3 Boot"
-@cecho off 
-cecho {04}1=Windows{\n}2=PS3{03}
-cecho {07} Install.usb on PS3
-set /a m=choose one;
+@echo off 
+@set "cecho.\cecho.exe"
+%cecho% {04}1=Windows{\n}2=PS3{03}
+%cecho% {07} Install.usb on PS3
+set /p m=choose one;
 if (%m%=1) goto one
 if (%m%=2) goto two
 goto top
@@ -26,6 +27,6 @@ copy "%folder2%%iso2% ./%rom2%"
 goto 7L
 
 :7L
-cecho {05}Made_By_Kia7L{02}
+%cecho% {05}Made_By_Kia7L{02}
 @timeout /t 5
 goto top
