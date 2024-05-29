b2593fbd7a3db29aa34f0861dae6bae66116c62e
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-29 01:05:14 -1000

    Update PS3-BY_7L.cmd

--- PS3-BY_7L.cmd
+++ PS3-BY_7L.cmd
@@ -9,7 +9,7 @@ title "PS3-BY_KA7L"
 @set "apk=191236791230"
 @set "app=198726310541"
 @set "exe=67184095Q236"
-%cecho% {04}1=PS3.apk{\n}2=PS3.app{\n}3=PS3.exe{07}
+%cecho% {04}1=PS3.apk{\n}2=PS3.app{\n}3=PS3.exe{07}{\n}
 
 set /p m=Choose;
 if (%m%=1) goto one
@@ -18,15 +18,15 @@ if (%m%=3) goto three
 goto top
 
 :one
-copy "%apk% ./PS3.apk"
+copy "%apk%" "./PS3.apk"
 goto 7L
 
 :two
-copy "%app% ./PS3.app"
+copy "%app%" "./PS3.app"
 goto 7L
 
 :three
-copy "%exe% ./PS3.exe"
+copy "%exe%" "./PS3.exe"
 goto 7L
 
 :7L
