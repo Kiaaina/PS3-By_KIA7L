f7738b343ec3768c31122c6cce05afacf98d03fa
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-29 01:06:48 -1000

    Update PS3_Boot.cmd

--- PS3_Boot.cmd
+++ PS3_Boot.cmd
@@ -5,7 +5,7 @@ mode 20,30
 title "PS3 Boot"
 @echo off 
 @set "cecho.\cecho.exe"
-%cecho% {04}1=Windows{\n}2=PS3{03}
+%cecho% {04}1=Windows{\n}2=PS3{03}{\n}
 %cecho% {07} Install.usb on PS3
 set /p m=choose one;
 if (%m%=1) goto one
@@ -16,14 +16,14 @@ goto top
 @set "folder1=.\Windows.iso\"
 set /p iso1=name.windows.iso;
 set /p rom1=name.windows.rom;
-copy "%folder1%%iso1% ./%rom1%"
+copy "%folder1%%iso1%" "./%rom1%"
 goto 7L
 
 :two
 @set "folder2=.\PS3.iso\"
 set /p iso2=name.game.iso;
 set /p rom2=name.game.rom;
-copy "%folder2%%iso2% ./%rom2%"
+copy "%folder2%%iso2%" "./%rom2%"
 goto 7L
 
 :7L
